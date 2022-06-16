35bce71a8b26e16dfa442c853d44237f3f490e68
elcho <elcho@LAPTOP-RDCES2UH>
2022-06-16 14:25:00 -0600

    El anexo de la clase Alumno

--- src/Battleship/Ejecutar_Juego.java
+++ src/Battleship/Ejecutar_Juego.java
@@ -4,29 +4,96 @@ public class Ejecutar_Juego {
     public static void main(String[] args) {
         Scanner lea=new Scanner(System.in);
          
-        String nombre="",contraseña="";
-        int puntos=0;
+        String nombre="",contraseña="",nombre2="",contraseña2="";
+        String Player1="";
+        String contraseñaPlayer1="";
+        String Player2="";
+        int puntos=0,respuesta=0,respuesta2=0,respuesta3=0;
         database crear=new database(nombre,contraseña,puntos);
-        boolean Menu_Inicio=false,Bcrear=false;
-        int Opcion_Menu=0;
+        boolean Menu_Inicio=false,Menu_prin=false;
+        int Opcion_Menu=0,Opcion_Prin=0;
+        
+        //Menu Inicio
         do{
             crear.imprimirMenuIn();
             Opcion_Menu=lea.nextInt();
-            
             switch(Opcion_Menu){
                 case 1:
                     crear.imprimirDatabase();
                     System.out.println("**Login**");
                     System.out.print("Username: ");
                     nombre=lea.next();
-                    System.out.println("Contraseña: ");
+                    System.out.print("Password: ");
                     contraseña=lea.next();
                     crear.login(nombre, contraseña);
                     crear.entrar(nombre,contraseña);
                     //Menu principal
                     if (crear.login(nombre, contraseña)==true){
+                         Player1=nombre;
+                         contraseñaPlayer1=contraseña;
+                        do{            
                         crear.imprimirMenuPrin();
-                        
+                        Opcion_Prin=lea.nextInt();
+                        switch(Opcion_Prin){
+                            case 1:
+                                System.out.println("--Inicio de Sesion para Player 2--");
+                                System.out.println("Username: ");
+                                nombre2=lea.next();
+                                System.out.println("Password: ");
+                                contraseña2=lea.next();
+                                crear.loginPlayer2(Player1, nombre2, contraseña2);
+                                if (crear.loginPlayer2(Player1, nombre2,contraseña2)==true){
+                                    Player2=nombre2;
+                                    System.out.println("Player 1: "+Player1);
+                                    System.out.println("Player 2: "+Player2);
+                                    System.out.println("Listos?");
+                                    System.out.println("1-SI  2-NO");
+                                    respuesta=lea.nextInt();
+                                }
+                                else{
+                                    System.out.println("Username o Password Incorrectos");
+                                    System.out.println("Desea volver a intentarlo?");
+                                    System.out.println("1-SI  2-NO");
+                                    respuesta2=lea.nextInt();
+                                    switch(respuesta2){
+                                        case 1:
+                                            System.out.println("-----------------");
+                                            break;
+                                        case 2:
+                                            System.out.println("Desea volver al Menu Principal?");   
+                                            System.out.println("1-SI   2-NO");
+                                            respuesta3=lea.nextInt();
+                                            switch(respuesta3){
+                                                case 1: 
+                                                    System.out.println("Estoy en el switch ");
+                                                    break;
+                                                case 2:
+                                                    System.out.println("--------------");
+                                                    break;
+                                            }
+                                            break;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                crear.imprimirConf();
+                                break;
+                            case 3:
+                                crear.imprimirRep();
+                                break;
+                            case 4:
+                                crear.imprimirPerf();
+                                break;
+                            case 5:
+                                System.out.println("--Volviendo al Menu de Inicio--");
+                                Menu_prin=true;
+                                break;
+                            default:
+                                System.out.println("---Opcion Invalida---");
+                                break;
+                        }
+                        }while(!Menu_prin);
+                        Menu_prin=false;
                     }
                     break;
                 case 2:
--- src/Battleship/database.java
+++ src/Battleship/database.java
@@ -73,9 +73,28 @@ public class database {
          }   
          return false;
      }
+     
+     public boolean loginPlayer2(String idPlayer1,String idPlayer2,String password2){
+            for (int i=0;i<id.size();i++){
+                if (id.get(i).equals(idPlayer2)){
+                        if (contra.get(i).equals(password2)&&idPlayer1!=idPlayer2){
+                            return true;
+                        }
+                }
+            }        
+         return false;
+     }
      public boolean entrar(String id2,String password){
           if (login(id2,password)==true){
-              System.out.println("Acceso concedido");
+              System.out.println("Bienvenido "+id2+" a Battleship");
+          return true;
+          }
+          System.out.println("Tu username o password son incorrectos");
+          return false;
+     }
+      public boolean entrarPlayer2(String idPlayer1,String idPlayer2,String password2){
+          if (loginPlayer2(idPlayer1,idPlayer2,password2)==true){
+              System.out.println("Player 2 Listo");
           return true;
           }
           System.out.println("Tu username o password son incorrectos");
